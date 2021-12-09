6d743b14db57cc4faf9d7553838d4c34664f1e4f
Zainab <ajumobizainab@gmail.com>
2021-12-09 02:35:47 +0100

    added dark mode feature

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (window.matchMedia('(prefers-color-scheme: dark)').matches){
+    document.documentElement.setAttribute('dark', true)
+}
+
 const app = Vue.createApp({
     data(){
         return {
@@ -12,16 +16,16 @@ const app = Vue.createApp({
     methods: {
         async getUser(){
             const res = await fetch('https://randomuser.me/api') 
-            const { results } = await res.json()
+            const { data } = await res.json()
 
             // console.log(results)
 
-            this.firstName = results[0].name.first
-            this.lastName = results[0].name.last
-            this.gender = results[0].gender
-            this.email = results[0].email
-            this.phone = results[0].phone
-            this.picture = results[0].picture.large
+            this.firstName = data[0].name.first
+            this.lastName = data[0].name.last
+            this.gender = data[0].gender
+            this.email = data[0].email
+            this.phone = data[0].phone
+            this.picture = data[0].picture.large
         },
     },
 })
